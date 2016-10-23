fb35ebc3e6542fed75969fbe80f96be4a9c4e798
Mike Mikina <mike@mikina.pl>
2016-10-23 23:53:01 +0200

    Running swiftlint autocorrect.

--- iOSProjectStarter/iOSProjectStarter/SupportingFiles/AppDelegate.swift
+++ iOSProjectStarter/iOSProjectStarter/SupportingFiles/AppDelegate.swift
@@ -13,13 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
-    
+
     // test warnings as error
     _ = 10
-    
+
     return true
   }
 
@@ -45,6 +44,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
   }
 
-
 }
-
