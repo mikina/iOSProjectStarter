ed856b69239c8065e747b62c0d24a91b36d62911
Mike Mikina <mike@mikina.pl>
2016-10-24 23:42:32 +0200

    Remove Storyboards.

--- iOSProjectStarter/iOSProjectStarter/SupportingFiles/AppDelegate.swift
+++ iOSProjectStarter/iOSProjectStarter/SupportingFiles/AppDelegate.swift
@@ -14,10 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
 
-    // test warnings as error
-    _ = 10
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+
+    let mainVC = ViewController()
+    self.window?.rootViewController = mainVC
+    self.window?.makeKeyAndVisible()
 
     return true
   }
